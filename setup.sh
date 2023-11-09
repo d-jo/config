623873dca8921d6176b106b1b8df7b26b67d24d5
djo <declan.l.johnson@gmail.com>
2023-11-08 23:15:07 -0600

    neovim, kitty

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
-echo "Setting up tmux.conf"
-ln ./tmux.conf ~/.tmux.conf
-echo "Setting up vimrc"
-ln ./vimrc ~/.vimrc
 echo "Setting up kitty.conf"
+mkdir -p ~/.config/kitty
 ln ./kitty.conf ~/.config/kitty/kitty.conf
-echo "Setting up VimCompletesMe"
-git clone git://github.com/ajh17/VimCompletesMe.git ~/.vim/pack/vendor/start/VimCompletesMe
-echo "Setting up vim-plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Setting up init.vim for neovim"
+mkdir -p ~/.config/nvim
+ln ./init.lua ~/.config/nvim/init.lua
